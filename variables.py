d801859ba32b305aa2bd46fa718741bd72a71487
DragonFly378 <hafichever@gmail.com>
2023-03-26 14:12:11 +0700

    variables topic

---
+++ variables.py
@@ -0,0 +1,11 @@
+x = 1               # int
+y = 2.4             # float
+name = 'hafiz'      # str
+is_correct = False  # bool
+
+# Multiple Assignment
+a, b, name, is_done = ( 4, 5, 'joko', True)
+
+x = str(x)
+
+print(type(y))
