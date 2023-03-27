a644bae6687c729c57cef1c2a22b7bf633ef9965
DragonFly378 <hafichever@gmail.com>
2023-03-27 16:47:09 +0700

    for loops topic

---
+++ loops.py
@@ -0,0 +1,39 @@
+
+# ==> for loops
+
+peoples = ['hafiz', 'shafira', 'kiki', 'ardi']
+
+## Simple for loops
+# for person in peoples:
+#     print(f"Orang sekarang adalah {person}")
+
+## With Index
+# for idx, person in enumerate(peoples):
+#     print(f"Orang ke : {idx} adalah {person}")
+
+## Break
+# for person in peoples:
+#     if person == 'kiki':
+#         break
+#     print(f"Orang sekarang adalah {person}")
+
+## Continue
+# for person in peoples:
+    # if person == 'shafira':
+    #     continue
+    # print(f"Orang sekarang adalah {person}")
+
+## Range
+# for i in range(len(peoples)):  
+#     print(f"Orang ke-{i} adalah: {peoples[i]}")
+
+# for i in range(0, 12):  
+#     print(f"Orang ke-{i}")
+
+
+# ==> While condition
+count = 0 
+
+while count < 10:
+    print(f'Count: {count}')
+    count += 1
