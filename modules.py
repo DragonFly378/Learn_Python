c6535b733902668a44c179537031ba1a5255fd21
DragonFly378 <hafichever@gmail.com>
2023-03-30 06:58:19 +0700

    update custom modules from other file

--- modules.py
+++ modules.py
@@ -6,12 +6,23 @@ from datetime import date
 # pip modules
 from camelcase import CamelCase
 
+# Custom Modules using validator.py
+# import validator 
+#   or we can write like this
+from validator import validate_email
+
 # today = datetime.date.today()
 today = date.today()
 
 # inisiasi p menjadi objek
 p = CamelCase()
 
-print(p.hump("halo semua, aku sudah datang"))
+# print(p.hump("halo semua, aku sudah datang"))
+# print(today)
 
-print(today)
+email = "capung23@gmail.com"
+
+if validate_email(email):
+    print("Email valid gan!")
+else:
+    print("Email rusak gan!")
